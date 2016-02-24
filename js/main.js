0d9ff34ff8b34ab671475610e3e5ffa14c86b89f
David G. Shannon <david.shannon@empaytech.com>
2016-02-24 19:30:32 +0100

    added broadcaster code

--- js/main.js
+++ js/main.js
@@ -18,14 +18,16 @@ function initWebRTC() {
         number: "VIEWER-" + new Date,
         publish_key   : 'pub-c-f9b642ff-c435-4519-b121-78d72a7b4c5e',
         subscribe_key : 'sub-c-3e8402ec-d9a1-11e5-8758-02ee2ddab7fe',
-        ssl: true
+        ssl: true,
+        media: {video: true}
     });
 
     // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     // Join a Broadcast as a Viewer
     // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     viewer.ready(function () {
-        var broadcaster = phone.dial("BROADCASTER");
+        var broadcaster = viewer.dial("BROADCASTER");
+        broadcaster.send("Lol");
     });
 
     // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -33,9 +35,10 @@ function initWebRTC() {
     // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     viewer.receive(function (broadcaster) {
         broadcaster.connected(function (broadcaster) {
-            document.body.appendChild(broadcaster.video);
+            document.getElementById("broadcaster").src = broadcaster.video.src;
         });
         broadcaster.ended(function (broadcaster) { /* broadcast ended */ });
+        
     });
 }
 
